6e0302902db0b95cdcdb94b948c03ed47b29f262
tantexian@qq.com <tantexian@163.com>
2016-03-15 13:43:10 +0800

    增加head 代理模式

---
+++ src/main/java/head/proxy/Client.java
@@ -0,0 +1,20 @@
+package head.proxy;
+
+/**
+ * TODO 代理模式：为另一个对象提供替身或占位符以控制对这个对象的访问
+ * 
+ * ---- 代理模式有多种实现方式：
+ *   -- 1、其中最简单的是使用Java的远程RMI代理实现。集成rmi.remote接口。
+ *         使用lookup函数查找service服务，再service.sayHello()即可调用（本地调用远程堆的对象，通过网络返回）。
+ *   -- 2、
+ *   
+ *   
+ * @author ttx
+ * @since 2016年3月15日 上午11:02:40
+ */
+public class Client {
+
+	public static void main(String[] args) {
+		
+	}
+}
